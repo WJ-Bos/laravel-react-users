706744c3b2b9adb2ee7f13b10964b10d2cf37dfd
WJ-Boshoff <willemwors15@gmail.com>
2024-09-13 01:20:12 +0200

    Created Signup page and added a cool ass waving hand

--- react-frontend/src/views/Login.jsx
+++ react-frontend/src/views/Login.jsx
@@ -21,15 +21,16 @@ function Login(props) {
   }
 
   return (
-    <div className="flex bg-neutral-900 h-screen items-center justify-center text-white relative">
+    <div className="flex flex-col bg-neutral-900 h-screen items-center justify-evenly text-white relative">
+      <h1 className="text-4xl font-semibold max-w-[450px]">Hi there <span className="waving-hand">👋🏻</span></h1>
       <form
-        className="flex w-[60%] relative flex-col sm:max-w-[40%] md:max-w-[40%] lg:max-w-[40%] rounded-sm p-2 border-white border-[1px] border-opacity-60 shadow-bottom-right shadow-neutral-500">
-        <div className="flex flex-col mb-4">
-          <h1 className="font-bold text-xl pt-2 pl-3">Sign In</h1>
-          <label className="font-normal text-xs pl-3 text-neutral-400">Sign in to an existing account</label>
+        className="flex w-[90%] relative flex-col sm:w-[90%] md:w-[70%] lg:w-[50%] rounded-sm p-2 border-white border-[1px] border-opacity-60 shadow-bottom-right shadow-neutral-500 h-[350px] mb-10">
+      <div className="flex flex-col mb-4">
+          <h1 className="font-bold text-2xl pt-2 pl-3">Sign In</h1>
+          <label className="font-normal text-xs pl-3 text-neutral-400 mt-[4px]">Sign in to an existing account</label>
         </div>
         <div className="mt-4 flex flex-col">
-          <label className="relative text-sm ml-3 text-white font-semibold mb-[2px]">Email</label>
+          <label className="relative text-md ml-3 text-white font-semibold mb-[2px]">Email</label>
           <input
             className="w-[90%] ml-3 h-8 rounded-sm p-2 mb-2 bg-neutral-900 border-white border-[1px] border-opacity-60"
             type="text"
@@ -37,7 +38,7 @@ function Login(props) {
             onChange={handleEmailChange}/>
         </div>
         <div className="mt-2 flex flex-col">
-          <label className="relative font-semibold ml-3 text-sm text-white mb-[2px]">Password</label>
+          <label className="relative font-semibold ml-3 text-md text-white mb-[2px]">Password</label>
           <input
             className="w-[90%] ml-3 h-8 rounded-sm order-white border-[1px] border-opacity-60 p-2 mb-2 bg-neutral-900"
             type="password"
@@ -46,7 +47,7 @@ function Login(props) {
         </div>
         <div className="flex justify-center">
           <button
-            className="p-2 w-[80px] bg-white font-semibold text-black rounded-md sm:w-[100px] text-sm mt-6 mb-2 hover:bg-neutral-300 duration-300"
+            className="p-2 w-[80px] bg-white font-semibold text-black rounded-md sm:w-[100px] text-sm mt-10 mb-2 hover:bg-neutral-300 duration-300"
             onClick={handleSubmit}>Sign In
           </button>
         </div>
--- react-frontend/src/views/SignUp.jsx
+++ react-frontend/src/views/SignUp.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function SignUp(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+
+    setEmail("");
+    setPassword("");
+    setName("");
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+  }
+
+
   return (
-    <div>
-      Sign Up
+    <div className="flex bg-neutral-900 h-screen items-center justify-center text-white relative">
+      <form
+        className="flex w-[60%] relative flex-col sm:max-w-[40%] md:max-w-[40%] lg:max-w-[40%] rounded-sm p-2 border-white border-[1px] border-opacity-60 shadow-bottom-right shadow-neutral-500">
+        <div className="flex flex-col mb-4">
+          <h1 className="font-bold text-xl pt-2 pl-3">Sign Up</h1>
+          <label className="font-normal text-xs pl-3 text-neutral-400 mt-[4px]">Create a new account</label>
+        </div>
+        <div className="mt-4 flex flex-col">
+          <label className="relative text-sm ml-3 text-white font-semibold mb-[2px]">Name</label>
+          <input
+            className="w-[90%] ml-3 h-8 rounded-sm p-2 mb-2 bg-neutral-900 border-white border-[1px] border-opacity-60"
+            type="text"
+            value={name}
+            onChange={handleNameChange}/>
+        </div>
+        <div className="mt-2 flex flex-col">
+          <label className="relative font-semibold ml-3 text-sm text-white mb-[2px]">Email</label>
+          <input
+            className="w-[90%] ml-3 h-8 rounded-sm order-white border-[1px] border-opacity-60 p-2 mb-2 bg-neutral-900"
+            type="text"
+            value={email}
+            onChange={handleEmailChange}/>
+        </div>
+        <div className="mt-2 flex flex-col">
+          <label className="relative font-semibold ml-3 text-sm text-white mb-[2px]">Password</label>
+          <input
+            className="w-[90%] ml-3 h-8 rounded-sm order-white border-[1px] border-opacity-60 p-2 mb-2 bg-neutral-900"
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}/>
+        </div>
+        <div className="flex justify-center">
+          <button
+            className="p-2 w-[80px] bg-white font-semibold text-black rounded-md sm:w-[100px] text-sm mt-6 mb-2 hover:bg-neutral-300 duration-300"
+            onClick={handleSubmit}>Save
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
