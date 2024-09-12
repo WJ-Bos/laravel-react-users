97bbb8ebcb6c66c1402fdf4e4786cbd5a53ec17e
WJ-Boshoff <willemwors15@gmail.com>
2024-09-12 11:54:15 +0200

    Different Layout boilerplate

---
+++ react-frontend/src/components/DefaultLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DefaultLayout(props) {
+  return (
+    <div></div>
+  );
+}
+
+export default DefaultLayout;
---
+++ react-frontend/src/components/GuestLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GuestLayout(props) {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default GuestLayout;
