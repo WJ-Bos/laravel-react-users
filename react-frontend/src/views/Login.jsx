c67a7c8b407910098667c40544ccb79f48d936d7
WJ-Boshoff <willemwors15@gmail.com>
2024-09-16 20:35:38 +0200

    Added ref instead of state and Axios client

--- react-frontend/src/views/Login.jsx
+++ react-frontend/src/views/Login.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { Link } from 'react-router-dom'
 
 function Login(props) {
 
@@ -7,7 +8,6 @@ function Login(props) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(email, password);
 
     setEmail("");
     setPassword("");
@@ -24,7 +24,7 @@ function Login(props) {
     <div className="flex flex-col bg-neutral-900 h-screen items-center justify-evenly text-white relative">
       <h1 className="text-4xl font-semibold max-w-[450px]">Hi there <span className="waving-hand">👋🏻</span></h1>
       <form
-        className="flex w-[90%] relative flex-col sm:w-[90%] md:w-[70%] lg:w-[50%] rounded-sm p-2 border-white border-[1px] border-opacity-60 shadow-bottom-right shadow-neutral-500 h-[350px] mb-10">
+        className="flex w-[90%] relative flex-col sm:w-[90%] md:w-[70%] lg:w-[50%] rounded-sm p-6 border-white border-[1px] border-opacity-60 shadow-bottom-right shadow-neutral-500 mb-10">
       <div className="flex flex-col mb-4">
           <h1 className="font-bold text-2xl pt-2 pl-3">Sign In</h1>
           <label className="font-normal text-xs pl-3 text-neutral-400 mt-[4px]">Sign in to an existing account</label>
@@ -51,6 +51,7 @@ function Login(props) {
             onClick={handleSubmit}>Sign In
           </button>
         </div>
+        <p className='text-center p-3 text-neutral-500 text-lg'> Not Registered ? <Link to="/signup" className="p-3 text-white">Create an account</Link></p>
       </form>
     </div>
   );
--- react-frontend/src/views/SignUp.jsx
+++ react-frontend/src/views/SignUp.jsx
@@ -1,35 +1,43 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
+import {Link} from "react-router-dom";
+import users from "./Users.jsx";
+import axiosClient from "../axios-client.js";
+import {useStateContext} from "../context/ContextProvider.jsx";
 
 function SignUp(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
+
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef =useRef();
+  const confirmPasswordRef =useRef();
+
+  const  {setUser,setToken} = useStateContext();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(email, password);
-
-    setEmail("");
-    setPassword("");
-    setName("");
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      confirm_password: confirmPasswordRef.current.value
+    }
+    axiosClient.post('signup',payload)
+      .then(({data}) =>{
+        setUser(data.user);
+        setToken(data.token);
+      })
+      .catch((err) =>{
+        const response = err.response;
+        if(response && response.status === 422){ //422 validation error
+         console.log( response.data.errors);
+        }
+      })
   }
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value)
-  }
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value)
-  }
-
-  const handleNameChange = (e) => {
-    setName(e.target.value)
-  }
-
 
   return (
     <div className="flex bg-neutral-900 h-screen items-center justify-center text-white relative">
       <form
-        className="flex w-[60%] relative flex-col sm:max-w-[40%] md:max-w-[40%] lg:max-w-[40%] rounded-sm p-2 border-white border-[1px] border-opacity-60 shadow-bottom-right shadow-neutral-500">
+        className="flex w-[60%] relative flex-col sm:max-w-[40%] md:max-w-[40%] lg:max-w-[40%] rounded-sm p-4 border-white border-[1px] border-opacity-60 shadow-bottom-right shadow-neutral-500">
         <div className="flex flex-col mb-4">
           <h1 className="font-bold text-xl pt-2 pl-3">Sign Up</h1>
           <label className="font-normal text-xs pl-3 text-neutral-400 mt-[4px]">Create a new account</label>
@@ -37,26 +45,32 @@ function SignUp(props) {
         <div className="mt-4 flex flex-col">
           <label className="relative text-sm ml-3 text-white font-semibold mb-[2px]">Name</label>
           <input
+            ref={nameRef}
             className="w-[90%] ml-3 h-8 rounded-sm p-2 mb-2 bg-neutral-900 border-white border-[1px] border-opacity-60"
             type="text"
-            value={name}
-            onChange={handleNameChange}/>
+          />
         </div>
         <div className="mt-2 flex flex-col">
           <label className="relative font-semibold ml-3 text-sm text-white mb-[2px]">Email</label>
           <input
+            ref={emailRef}
             className="w-[90%] ml-3 h-8 rounded-sm order-white border-[1px] border-opacity-60 p-2 mb-2 bg-neutral-900"
             type="text"
-            value={email}
-            onChange={handleEmailChange}/>
+          />
         </div>
         <div className="mt-2 flex flex-col">
           <label className="relative font-semibold ml-3 text-sm text-white mb-[2px]">Password</label>
           <input
+            ref={passwordRef}
             className="w-[90%] ml-3 h-8 rounded-sm order-white border-[1px] border-opacity-60 p-2 mb-2 bg-neutral-900"
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}/>
+            type="password"/>
+        </div>
+        <div className="mt-2 flex flex-col">
+          <label className="relative font-semibold ml-3 text-sm text-white mb-[2px]">Confirm Password</label>
+          <input
+            ref={confirmPasswordRef}
+            className="w-[90%] ml-3 h-8 rounded-sm order-white border-[1px] border-opacity-60 p-2 mb-2 bg-neutral-900"
+            type="password"/>
         </div>
         <div className="flex justify-center">
           <button
@@ -64,6 +78,9 @@ function SignUp(props) {
             onClick={handleSubmit}>Save
           </button>
         </div>
+        <p className='text-center pr-2 text-neutral-500 text-lg'> Already Registered ? <Link to="/login"
+                                                                                             className="pl-2 text-white">Sign
+          in .</Link></p>
       </form>
     </div>
   );
