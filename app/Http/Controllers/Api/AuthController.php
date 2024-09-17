4074a9f98a4846edf22a471526c128b543ee60bf
WJ-Boshoff <willemwors15@gmail.com>
2024-09-17 12:00:39 +0200

    finished login and logout controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,15 +7,22 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    /**
+     * Handles the sign up request.
+     * Validates the request data and creates a new user.
+     * Creates a new token for the user.
+     * Returns the user and the token in the response.
+     *
+     * @param  \App\Http\Requests\SignupRequest  $request
+     * @return \Illuminate\Http\Response
+     */
     public function signup(SignupRequest $request){
-
-        //validating the data from request
         $data = $request->validated();
 
-        //create a user using request data
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
@@ -24,15 +31,39 @@ class AuthController extends Controller
 
         //creating a token
         $token = $user->createToken('main')->plainTextToken;
-
         return compact('user', 'token');
     }
 
-    public function login(LoginRequest $request){
 
+
+    /**
+     * Handles the login request.
+     * Validates the request data and logs the user in.
+     * Creates a new token for the user.
+     * Returns the user and the token in the response.
+     *
+     * @param  \App\Http\Requests\LoginRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(LoginRequest $request){
+        $credentials = $request->validated();
+        if(!Auth::attempt($credentials)){
+            return response([
+                'message' => 'Provided credentials are not correct'
+            ]);
+        }
+        /** @var User $user */
+        $user = Auth::user();
+        $user->createToken('main')->plainTextToken;
+        return compact('user','token');
     }
 
-    public  function logout(Request $request){
 
+
+    public  function logout(Request $request){
+        /**@var User $user*/
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return response('', 204);
     }
 }
